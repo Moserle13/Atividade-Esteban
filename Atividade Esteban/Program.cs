9d43585cafd83f255478be3c7a031aec6fdd38b8
Germano Moserle <gemoserle22@gmail.com>
2025-03-31 19:43:32 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade Esteban/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class BuscaSequencial
+{
+    public static int BuscarElemento(int[] array, int elemento)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == elemento)
+                return i;
+        }
+        return -1; // Retorna -1 se não encontrar
+    }
+
+    public static void Main()
+    {
+        int[] arr = { 1, 3, 5, 7, 9 };
+        int elemento = 7;
+        int indice = BuscarElemento(arr, elemento);
+
+        Console.WriteLine(indice == -1 ? "Elemento não encontrado" : $"Elemento encontrado no índice {indice}");
+    }
+}
